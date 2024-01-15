82be90029a3e27b406c031710f0c0cd613629b6b
NicolasV99 <legolasnicolas99@gmail.com>
2024-01-14 19:05:48 -0500

    Prep 2 exercise

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+class Program{
+    static void Main(string[] args){
+        // Step 1: Ask the user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        double gradePercentage = Convert.ToDouble(Console.ReadLine());
+
+        // Step 2: Determine the letter grade using if-elif-else statements
+        char letter;
+
+        if (gradePercentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        // Step 3: Display the letter grade
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        // Step 4: Check if the user passed the course and display a message
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep it up! You can improve next time.");
+        }
     }
 }
