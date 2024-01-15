de05a83f483ccf29e9219403145591e29e9e8dae
NicolasV99 <legolasnicolas99@gmail.com>
2024-01-14 19:19:35 -0500

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+class Program{
+    static void Main(string[] args){
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userInput;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+
+        } while (userInput != 0);
+
+        // Core Requirement 1: Compute the sum of the numbers
+        int sum = numbers.Sum();
+
+        // Core Requirement 2: Compute the average of the numbers
+        double average = numbers.Average();
+
+        // Core Requirement 3: Find the maximum number in the list
+        int maxNumber = numbers.Max();
+
+        // Display the results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
