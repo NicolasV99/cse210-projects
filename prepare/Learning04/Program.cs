674baf38e2a0c859b34c22be906cd95ac7650ee0
NicolasV99 <legolasnicolas99@gmail.com>
2024-01-27 16:46:12 -0500

    Learning04-completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,89 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+    }
+}
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    // We will provide Getters for our private member variables so they can be accessed
+    // later both outside the class as well is in derived classes.
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+}
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    // Notice the syntax here that the MathAssignment constructor has 4 parameters and then
+    // it passes 2 of them directly to the "base" constructor, which is the "Assignment" class constructor.
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        // Here we set the MathAssignment specific variables
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    // Notice the syntax here that the WritingAssignment constructor has 3 parameters and then
+    // it passes 2 of them directly to the "base" constructor, which is the "Assignment" class constructor.
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        // Here we set any variables specific to the WritingAssignment class
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        // Notice that we are calling the getter here because _studentName is private in the base class
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
     }
 }
